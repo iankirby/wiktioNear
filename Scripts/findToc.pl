ac052400659439913cb7d2dd5c21963569905c9f
iankirby <55120975+iankirby@users.noreply.github.com>
2022-04-18 14:08:26 -0400

    things

---
+++ Scripts/findToc.pl
@@ -0,0 +1,17 @@
+#/usr/bin/perl
+
+use strict; use warnings; use utf8;
+
+#First, verify that there is a command line argument.
+if ($#ARGV<0){die "Requires a command line argument";}
+
+#Read the file in as a command line argument
+my $filename="$ARGV[0]";
+$filename="../FilesOut/".$filename;
+open (my $fh, '<: encoding(UTF-8)', $filename) or die "Cannot open $filename $!";
+
+my $raw="";
+
+while (my $row=<$fh>){$raw=$raw.$row;}
+
+print $raw
