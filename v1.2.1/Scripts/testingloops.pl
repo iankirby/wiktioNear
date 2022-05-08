056913faf81b6a6c24afc7ca77cf4319e4b232f1
iankirby <55120975+iankirby@users.noreply.github.com>
2022-05-08 14:30:40 -0400

    testing environments

---
+++ v1.2.1/Scripts/testingloops.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use utf8;
+
+# my $i=0;
+# while ($i<1000){
+#     print $i;
+#     print "\n";
+#     $i++;
+# }
+
+
+my $x=100;
+
+if ($x<0){
+    print"first";
+} elsif($x>1){
+    print"second";
+} elsif($x>99){
+    print" third";
+}
